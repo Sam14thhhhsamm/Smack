3acc831d33c2ed15303b93fc8e68e684acd61807
Sam14thhhhsam <hossam-bibtexchina@hotmail.com>
2019-06-20 09:49:37 +0800

    Commit 2

---
+++ app/src/main/java/com/example/smack/LogInActivity.kt
@@ -0,0 +1,22 @@
+package com.example.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+    fun logInLogInBtn(view: View){
+
+    }
+
+    fun logInCreateUserBtn(view:View){
+
+    }
+
+
+}
